74cc7ab2a0448728a6dba4aae13b1d78825f2917
dhanoahs3 <dhanoahs3@gmail.com>
2016-04-21 10:21:00 -0600

    Update Test1.java

--- src/test/java/com/qtp/selenium/Test1.java
+++ src/test/java/com/qtp/selenium/Test1.java
@@ -1,14 +1,6 @@
 package com.qtp.selenium;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 
-import org.openqa.selenium.Platform;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Test1 {
